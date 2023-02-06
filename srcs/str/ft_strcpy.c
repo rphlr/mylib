82b95123cb843e2dbdac1d1d019d98cda0cf0e13
Raphael Rouiller <rrouille@c1r7s8.42lausanne.ch>
2023-02-06 13:08:25 +0100

    Auto-commit

--- srcs/str/ft_strcpy.c
+++ srcs/str/ft_strcpy.c
@@ -6,7 +6,7 @@
 /*   By: rrouille <rrouille@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/14 14:07:12 by rrouille          #+#    #+#             */
-/*   Updated: 2023/02/06 13:04:25 by rrouille         ###   ########.fr       */
+/*   Updated: 2023/02/06 13:08:24 by rrouille         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,25 +30,4 @@ char	*ft_strcpy(char *dest, const char *src)
 	}
 	dest[i] = '\0';
 	return (dest);
-}
-
-/**
- * @brief	Copies the string pointed to by `src` (including the null
- * 			terminator) to the buffer pointed to by `dest`.
- * @return	Returns a pointer to the destination string `dest`.
- * @param	char_ptr	Pointer to the destination buffer where the string is
- * 						to be copied.
- * @param	char_ptr	Pointer to the null-terminated string to be copied.
- */
-void ft_strcpy_void(char *dest, char *src)
-{
-	int	i;
-
-	i = 0;
-	while (src[i] != '\0')
-	{
-		dest[i] = src[i];
-		i++;
-	}
-	dest[i] = '\0';
 }
