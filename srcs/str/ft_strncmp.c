de9b076c744d5b223a140cef926c99cff1d3f97b
Raphael Rouiller <rrouille@c1r7s8.42lausanne.ch>
2023-02-17 10:59:59 +0100

    Auto-commit

--- srcs/str/ft_strncmp.c
+++ srcs/str/ft_strncmp.c
@@ -17,13 +17,13 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	size_t	i;
 
 	i = 0;
-	while (s1[i] != '\0' && s2[i] != '\0' && i < n)
+	while ((*s1 || s2[i]) && n > 0)
 	{
-		if (s1[i] != s2[i])
-			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
-		i++;
+		if (*s1 != *s2)
+			return ((unsigned char)*s1 - (unsigned char)*s2);
+		s1++;
+		s2++;
+		n--;
 	}
-	if (i == n)
-		return (0);
-	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+	return (0);
 }
