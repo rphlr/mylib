1096ab9ee6e934ea60a55a52eb743333eccd0dbc
Raphael Rouiller <rrouille@c1r7s8.42lausanne.ch>
2023-08-10 14:10:19 +0200

    norminette ok

--- srcs/str/ft_strncpy.c
+++ srcs/str/ft_strncpy.c
@@ -6,16 +6,16 @@
 /*   By: rrouille <rrouille@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/10 12:31:04 by rrouille          #+#    #+#             */
-/*   Updated: 2023/08/10 12:42:32 by rrouille         ###   ########.fr       */
+/*   Updated: 2023/08/10 14:09:56 by rrouille         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "mylib.h"
 
-char *ft_strncpy(char *dest, const char *src, size_t n)
+char	*ft_strncpy(char *dest, const char *src, size_t n)
 {
-	size_t i;
-	
+	size_t	i;
+
 	i = -1;
 	while (++i < n && src[i] != '\0')
 		dest[i] = src[i];
