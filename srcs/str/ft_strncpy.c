ec4cfd4c875dd25515941f5f5ed1bc00f16b36fb
Raphael Rouiller <rrouille@c1r7s8.42lausanne.ch>
2023-08-10 12:40:03 +0200

    added ft_strncpy

--- srcs/str/ft_strncpy.c
+++ srcs/str/ft_strncpy.c
@@ -6,20 +6,19 @@
 /*   By: rrouille <rrouille@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/10 12:31:04 by rrouille          #+#    #+#             */
-/*   Updated: 2023/08/10 12:33:56 by rrouille         ###   ########.fr       */
+/*   Updated: 2023/08/10 12:39:52 by rrouille         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "mylib.h"
 
-char	*ft_strncpy(char *dest, const char *src, size_t n)
-{
-	size_t	i;
-
-	i = -1;
-	while (i < n && src[i] != '\0')
-		dest[i] = src[i];
-	while (i++ < n)
-		dest[i] = '\0';
-	return (dest);
+char *ft_strncpy(char *dest, const char *src, size_t n) {
+    size_t i;
+    for (i = 0; i < n && src[i] != '\0'; i++) {
+        dest[i] = src[i];
+    }
+    for (; i < n; i++) {
+        dest[i] = '\0';
+    }
+    return dest;
 }
